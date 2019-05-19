6f0b444cb8cb57eee926752ac4ab2222a3938f98
Jethro Glaudin <jethroglaudin.student@careerdevs.com>
2019-05-18 21:21:57 -0400

    Arrow Functions

---
+++ Challenge06.js
@@ -0,0 +1,18 @@
+/* CHALLENGE 6 - Arrow Functions
+
+Use arrow functions instead of functions
+where possible.
+*/
+
+"use strict";
+const mult = (a, b) => a * b;
+
+
+// function mult(a, b) {
+//   return a * b;
+// }
+setTimeout(() => console.log(mult(5, 10)), 1000);
+// setTimeout(function() {
+//   console.log(mult(5, 10));
+// }, 1000);
+// 50
